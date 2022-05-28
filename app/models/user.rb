383569abf9d67cdbfc90e0fde0c0316aa7d808fb
yuya <yuya004847yy@gmail.com>
2022-05-28 23:40:07 +0900

    Add Email Custom Validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+  before_validation :downcase_email
+  
   has_secure_password
 
   validates :name, presence: true,
@@ -6,6 +10,10 @@ class User < ApplicationRecord
                      maximum: 30,
                      allow_blank: true
                    }
+
+  validates :email, presence: true,
+                    email: { allow_blank: true }
+
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
   validates :password, presence: true,
                       length: { minimum: 8,
@@ -17,4 +25,26 @@ class User < ApplicationRecord
                         allow_blank: true
                       },
                       allow_nil: true
+
+  ## methods
+  # class method  ###########################
+  class << self
+    # emailからアクティブなユーザーを返す
+    def find_by_activated(email)
+      find_by(email: email, activated: true)
+    end
+  end
+
+  # class method end #########################
+  # 自分以外の同じemailのアクティブなユーザーがいる場合にtrueを返す
+  def email_activated?
+    users = User.where.not(id: id)
+    users.find_by_activated(email).present?
+  end
+
+  private
+  # email小文字化
+  def downcase_email
+    self.email.downcase! if email
+  end
 end
---
+++ lib/validator/email_validator.rb
@@ -0,0 +1,14 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    # text length
+    max = 255
+    record.errors.add(attribute, :too_long, count: max) if value.length > max
+
+    # format
+    format = /\A\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*\z/
+    record.errors.add(attribute, :invalid) unless format =~ value
+
+    # uniqueness
+    record.errors.add(attribute, :taken) if record.email_activated?
+  end
+end
